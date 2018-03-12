16caff7b5d1a61399878e70c2d138845be06e723
Will Baumann <wbaumann@github.com>
2018-03-11 22:20:53 -0400

    Added feathericon support and added a 'user' icon to the account
    https://github.com/carmelopullara/react-feather

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { User } from 'react-feather';
 
 import '../../css/oswald.css'
 import '../../css/open-sans.css'
@@ -9,8 +10,9 @@ import './styles.css'
 class Account extends Component {
     render() {
         return(
-            <div>
-                <a href="#" className="pure-menu-heading pure-menu-link navbar-right">0x0123456789...</a>
+            <div className="navbar-right">
+                <User color="#ffffff" className="account"/>
+                <a href="#" className="pure-menu-heading pure-menu-link">0x0123456789...</a>
             </div>
         );
     }
