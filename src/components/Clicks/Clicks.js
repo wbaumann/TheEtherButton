add761b4a81963b38061881ecf223faebe7cef3a
Will Baumann <wbaumann@github.com>
2018-04-06 19:06:03 -0400

    Added styling for one or more clicks

--- src/components/Clicks/Clicks.js
+++ src/components/Clicks/Clicks.js
@@ -5,11 +5,17 @@ import './styles.css'
 
 class Clicks extends Component {
     render() {
+        const erc721Clicks = [
+            {blocksAwayFromDesiredBlock: 16, clickTime:1273185387, clickGeneration: 1},
+            {blocksAwayFromDesiredBlock: 11, clickTime:1276185387, clickGeneration: 2},
+            {blocksAwayFromDesiredBlock: 5, clickTime:1289185387, clickGeneration: 3},
+        ]
         return(
             <div>
-                <h2>Your Clicks</h2>
-                <p>Your Click Metadata Here</p>
-                <Erc721Flair blocksAwayFromDesiredBlock={10} clickTime={1273185387} clickGeneration={1} />
+                <h1>Your ERC-721 Click Tokens:</h1>
+                <div className="click-flairs" >
+                    {erc721Clicks.map(erc721Click => <Erc721Flair {...erc721Click} key={erc721Click.clickGeneration} /> )} 
+                </div>
             </div>
         );
     }
