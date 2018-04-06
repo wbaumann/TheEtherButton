9f58f4716e72e4e4392e68f090776f1ae014ecdb
Will Baumann <wbaumann@github.com>
2018-04-06 18:52:17 -0400

    Added support for an Erc721 click flair

--- src/components/Clicks/Clicks.js
+++ src/components/Clicks/Clicks.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
+import Erc721Flair from './Erc721Flair/Erc721Flair'
 
 import './styles.css'
 
-class Faq extends Component {
+class Clicks extends Component {
     render() {
         return(
             <div>
                 <h2>Your Clicks</h2>
                 <p>Your Click Metadata Here</p>
+                <Erc721Flair blocksAwayFromDesiredBlock={10} clickTime={1273185387} clickGeneration={1} />
             </div>
         );
     }
 }
 
-export default Faq
+export default Clicks
