0a77ff66ea2fb97fee6525866692d37c015df3d1
Will Baumann <wbaumann@github.com>
2018-04-09 16:23:33 -0400

    Created a standalone click flair section

---
+++ src/components/ClickFlairs/ClickFlairs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import Erc721Flair from './Erc721Flair/Erc721Flair'
+
+import './styles.css'
+
+class ClickFlairs extends Component {
+    render() {
+        return(
+            <div >
+                <div className="click-flairs" >
+                    {this.props.erc721Clicks.map(erc721Click => <Erc721Flair {...erc721Click} key={erc721Click.clickGeneration} /> )} 
+                </div>
+            </div>
+        );
+    }
+}
+
+ClickFlairs.propTypes = {
+    erc721Clicks: PropTypes.array
+};
+
+ClickFlairs.defaultProps = {
+    erc721Clicks: []
+};
+
+export default ClickFlairs
--- src/components/Clicks/Clicks.js
+++ src/components/Clicks/Clicks.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import Erc721Flair from './Erc721Flair/Erc721Flair'
+import React, { Component } from 'react';
+import ClickFlairs from '../ClickFlairs/ClickFlairs';
 
-import './styles.css'
+import './styles.css';
 
 class Clicks extends Component {
     render() {
         return(
             <div >
                 <h1>Your ERC-721 Click Tokens:</h1>
-                <div className="click-flairs" >
-                    {this.props.erc721Clicks.map(erc721Click => <Erc721Flair {...erc721Click} key={erc721Click.clickGeneration} /> )} 
-                </div>
+                <ClickFlairs erc721Clicks={this.props.erc721Clicks} />
             </div>
         );
     }
 }
-
-Clicks.propTypes = {
-    erc721Clicks: PropTypes.array
-};
-
-Clicks.defaultProps = {
-    erc721Clicks: []
-};
 
 export default Clicks
