34367b3392b9503d0c7221880cc9fbe02c2c4a44
Will Baumann <wbaumann@github.com>
2018-04-09 16:35:51 -0400

    Updated our footer to reflect our privacy policy

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,10 @@ class Footer extends Component {
         return(
             <div className="footer">
                 <ul>
-                    <li><a href="https://github.com/wbaumann/TheEtherButton">GitHub</a></li>
-                    <li><a href="#">EtherScan</a></li>
-                    <li><a href="https://opensource.org/licenses/MIT">License</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a target="_blank" href="https://github.com/wbaumann/TheEtherButton">GitHub</a></li>
+                    <li><a target="_blank" href="#">EtherScan</a></li>
+                    <li><a target="_blank" href="https://opensource.org/licenses/MIT">License</a></li>
+                    <li><a target="_blank" href="https://github.com/wbaumann/TheEtherButton/blob/master/privacy_policy.txt">Privacy Policy</a></li>
                 </ul>
             </div>
         );
