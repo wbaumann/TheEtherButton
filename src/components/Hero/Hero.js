5ae533f7519163656cc22773799039f9ae9da8d6
Will Baumann <wbaumann@github.com>
2018-04-19 18:09:56 -0400

    Updated the hero text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ class Hero extends Component {
     return (
       <div className="hero">
         <h1>An Ethereum-based version of Reddit&#39;s <a target="_blank" rel="noopener noreferrer" href="https://www.reddit.com/r/thebutton/">r/TheButton</a> game.</h1>
-        <p className="subtitle">You will be awarded a unique <a target="_blank" rel="noopener noreferrer" href="http://erc721.org/">ERC-721 token</a> flair based on how close the counter is to Block 0 when you click the button below, but you may only press it once. Choose wisely...</p>
+        <p className="subtitle">You will be awarded a unique <a target="_blank" rel="noopener noreferrer" href="http://erc721.org/">ERC-721 token</a> flair based on how close the counter is to Block 0 when you click the button below. This token is yours to sell, collect, or trade. But you may only press the button once. Choose wisely...</p>
       </div>
     );
   }
