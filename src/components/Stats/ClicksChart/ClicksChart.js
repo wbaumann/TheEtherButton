91611b68e316707fa320be1966acf3c6acc93f1d
Will Baumann <wbaumann@github.com>
2018-04-21 12:28:48 -0400

    Switched to using ChartJs in order to benefit from custom block colors

--- src/components/Stats/ClicksChart/ClicksChart.js
+++ src/components/Stats/ClicksChart/ClicksChart.js
@@ -1,34 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ChartistGraph from 'react-chartist';
+import { Bar } from 'react-chartjs-2';
+
+import GameColors from '../../../utils/GameColors';
 
 import './styles.css';
 
+const Color = require('color');
+
 class ClicksChart extends Component {
   render() {
+    const backgroundColor = [];
+    const borderColor = [];
+    const hoverBackgroundColor = [];
+    const hoverBorderColor = [];
     const labels = [];
-    const innerSeries = [];
-    const series = [];
+    const data = [];
 
     // eslint-disable-next-line
     for (let index = this.props.numberOfClicksAtBlocksRemaining.length - 1; index >= 0; index -= 1) {
+      const color = GameColors.getColor(index);
+      hoverBackgroundColor.push(color);
+      borderColor.push(color);
+      hoverBorderColor.push(color);
+      backgroundColor.push(Color(color).alpha(0.8).rgb().string());
       labels.push(`${index}`);
-      innerSeries.push(this.props.numberOfClicksAtBlocksRemaining[index]);
+      data.push(this.props.numberOfClicksAtBlocksRemaining[index]);
     }
 
-    series.push(innerSeries);
-    const data = { series, labels };
+    const chartData = {
+      labels,
+      datasets: [
+        {
+          backgroundColor,
+          borderColor,
+          borderWidth: 1,
+          hoverBackgroundColor,
+          hoverBorderColor,
+          data,
+        },
+      ],
+    };
 
     const options = {
-      axisY: {
-        onlyInteger: true,
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        enabled: false,
+      },
+      scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Block Number',
+          },
+        }],
       },
     };
 
     return (
-      <div>
-        <p>Distribution of all clicks:</p>
-        <ChartistGraph data={data} options={options} type="Bar" />
+      <div className="chart-separator">
+        <p className="chart-title">Distribution of all clicks:</p>
+        <div className="chart-holder">
+          <Bar data={chartData} options={options} />
+        </div>
       </div>
     );
   }
