5c78a382c4570b82a45a3b9bb6f31526b9a1626f
Will Baumann <wbaumann@github.com>
2018-05-31 18:01:05 -0400

    Added our etherscan contract address

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
       <div className="footer">
         <ul>
           <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/wbaumann/TheEtherButton">GitHub</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="#todo">EtherScan</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://etherscan.io/address/0xdeafc8d49ea6d6044eaa53f5e7c7c35c60e13d7f">EtherScan</a></li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://opensource.org/licenses/MIT">License</a></li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/wbaumann/TheEtherButton/blob/master/privacy_policy.txt">Privacy Policy</a></li>
         </ul>
